2e2108322e05be7efaf755a38dc07705690b1757
aitch_yoon <124541123+yooon613@users.noreply.github.com>
2025-02-13 12:33:22 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,11 @@
+def solution(s):
+    
+    if len(s)==4:
+        return s.isdigit()
+                
+    elif len(s)==6:
+        return s.isdigit()
+    
+    else:
+        return False
+        
