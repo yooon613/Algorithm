47c49b079cf75e8595b96fd9c6d7bf0289e0ecf8
aitch_yoon <124541123+yooon613@users.noreply.github.com>
2025-02-19 16:22:01 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    for i in range(1, n+1):
+        if i%2!=0:
+            answer.append(i)
+        else:
+            continue
+        
+        
+    return answer
